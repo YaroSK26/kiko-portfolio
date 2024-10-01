81657ed290dedc9bdeab014115a1a7df8938a11c
jaxo <jaroba0gmail.com>
2024-10-01 20:29:41 +0200

    :)

--- components/About.jsx
+++ components/About.jsx
@@ -13,8 +13,8 @@ export default function About() {
     >
       <h2 className="text-2xl font-semibold mb-4 text-center">About Me</h2>
       <p className="max-w-2xl mx-auto">
-        Felix Gray, a Slovak-Hungarian writer and photographer, is passionate about capturing the human experience's intricate details.<br /><br />
-        His work explores themes of loss, introspection, and transformation, focusing on life's darker, more emotional aspects.<br /><br />
+        Felix Gray, a Slovak-Hungarian writer and photographer, is passionate about capturing the human experience&apos;s intricate details.<br /><br />
+        His work explores themes of loss, introspection, and transformation, focusing on life&apos;s darker, more emotional aspects.<br /><br />
         Through his writing, Felix challenges societal norms and pushes storytelling boundaries. His narratives reflect personal and collective struggles, with a style that is both reflective and raw.<br /><br />
         As a photographer, he creates visual counterparts to his written work, exploring contrasts—light and shadow, beauty and decay, simplicity and complexity.<br /><br />
         His photographs serve as a visual commentary on his stories, capturing moments that speak to our silent emotions and untold stories. Each image aims to evoke a profound emotional response, drawing the viewer into the scene.<br /><br />
