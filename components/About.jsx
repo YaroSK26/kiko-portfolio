96bfc40260cc39ec7341d766a0c28ef1c8b71219
jaxo <jaroba0gmail.com>
2024-10-01 19:20:00 +0200

    about me

--- components/About.jsx
+++ components/About.jsx
@@ -9,13 +9,17 @@ export default function About() {
       animate={{ opacity: 1 }}
       transition={{ delay: 0.2 }}
       id="about"
-      className="text-center"
+      className=""
     >
-      <h2 className="text-2xl font-semibold mb-4">About Me</h2>
+      <h2 className="text-2xl font-semibold mb-4 text-center">About Me</h2>
       <p className="max-w-2xl mx-auto">
-        I&apos;m a passionate writer and poet, exploring the depths of human
-        emotion and experience through words. My work often reflects on nature,
-        love, and the complexities of modern life.
+        Felix Gray, a Slovak-Hungarian writer and photographer, is passionate about capturing the human experience's intricate details.<br /><br />
+        His work explores themes of loss, introspection, and transformation, focusing on life's darker, more emotional aspects.<br /><br />
+        Through his writing, Felix challenges societal norms and pushes storytelling boundaries. His narratives reflect personal and collective struggles, with a style that is both reflective and raw.<br /><br />
+        As a photographer, he creates visual counterparts to his written work, exploring contrasts—light and shadow, beauty and decay, simplicity and complexity.<br /><br />
+        His photographs serve as a visual commentary on his stories, capturing moments that speak to our silent emotions and untold stories. Each image aims to evoke a profound emotional response, drawing the viewer into the scene.<br /><br />
+        His portfolio reflects his desire to push artistic boundaries, blending the literary and visual into cohesive narratives. He is committed to evolving as an artist, experimenting with new styles and mediums.<br /><br />
+        His goals include expanding his body of work, engaging with diverse audiences, and contributing to the broader creative landscape with thought-provoking and evocative pieces.
       </p>
     </motion.section>
   );
